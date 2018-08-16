ed2937ffad722168ff26ba1332f9c1e0d39889bb
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 00:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function fuzzyMatch(drivers, letters) {
   return drivers.filter(driver => driver.startsWith(letters));
 };
 function matchName(drivers, name) {
-  return drivers.filter(driver => driver.name == "${name}");
+  return drivers.filter(driver => driver.name == name);
 };
